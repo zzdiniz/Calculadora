d008c3b0625bf3c4d74ba6bf1b2f262b852b805a
zzdiniz <bruno7240@gmail.com>
2023-01-08 17:07:26 -0300

    Calculadora Funcionando

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function calculate(type, value){
     console.log(type,value);
+    if(type==='action'){
+        if(value === 'clear'){
+            document.getElementById('result').value="";
+        }
+        else if( value ==="*" || value ==="/" || value ==="+" || value ==="-" || value ==="."){
+                document.getElementById('result').value+=value;
+        }
+        else if(value === '='){
+            let result=document.getElementById('result').value;
+            console.log(result);
+            document.getElementById('result').value=eval(result);
+        }
+
+    }
+    else if(type === 'value'){
+        document.getElementById('result').value+=value;
+    }
 }
